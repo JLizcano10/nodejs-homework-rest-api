1aaaf759fef6046a54712a8aa273dc63792c4bde
JL10 <jorge.lizcano410@gmail.com>
2024-02-21 14:33:06 -0500

    fix: patterns para name y phone de schema en schema/contacts.js

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -4,7 +4,7 @@ const schema = Joi.object({
   name: Joi.string()
     .min(3)
     .max(30)
-    .pattern(/^[A-Za-z] +$/)
+    .pattern(/^[A-Za-z\s]+$/)
     .messages({
       "string.pattern.base":
         "Invalid name, the name must be written only in letters and contain from 2 to 30",
@@ -20,7 +20,7 @@ const schema = Joi.object({
     })
     .required(),
   phone: Joi.string()
-    .pattern(/^\(\d{3}\) \d{3} -\d{4}$/)
+    .pattern(/^\(\d{3}\) \d{3}-\d{4}$/)
     .messages({
       "string.pattern.base":
         "Invalid phone number format, the format should be (XXX) XXX-XXXX",
