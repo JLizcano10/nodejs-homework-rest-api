1da18f3f7c091549001adbf16c0ba5b6c661b8a6
JL10 <jorge.lizcano410@gmail.com>
2024-02-26 21:49:05 -0500

    hw03 v1.0

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,31 +1,83 @@
 const Joi = require("joi");
+const { Schema, model } = require("mongoose");
 
+const namePattern = /^[A-Za-z\s]+$/;
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const phonePattern = /^\(\d{3}\) \d{3}-\d{4}$/;
+
+// Validacion con joi
 const schema = Joi.object({
   name: Joi.string()
     .min(3)
-    .max(30)
-    .pattern(/^[A-Za-z\s]+$/)
+    .max(70)
+    .pattern(namePattern)
     .messages({
       "string.pattern.base":
         "Invalid name, the name must be written only in letters and contain from 2 to 30",
     })
     .required(),
   email: Joi.string()
-    .email({
-      minDomainSegments: 2,
-      tlds: { allow: ["com", "net"] },
-    })
+    .pattern(emailPattern)
     .messages({
       "string.pattern.base": "Invalid email. the email must be valid",
     })
     .required(),
   phone: Joi.string()
-    .pattern(/^\(\d{3}\) \d{3}-\d{4}$/)
+    .pattern(phonePattern)
     .messages({
       "string.pattern.base":
         "Invalid phone number format, the format should be (XXX) XXX-XXXX",
     })
     .required(),
+  favorite: Joi.bool(),
 });
 
-module.exports = schema;
+// Validacion con Schema de mongoose. PEndiente seguir leyendo y mejorar?
+const contacts = new Schema(
+  {
+    name: {
+      type: String,
+      minLength: 3,
+      maxLength: 70,
+      required: [true, "Set name for contact"],
+      validate: {
+        validator: function (v) {
+          return namePattern.test(v);
+        },
+        message: (props) => `${props.value} is not a valid name`,
+      },
+    },
+    email: {
+      type: String,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return emailPattern.test(v);
+        },
+        message: (props) => `${props.value} is not a valid email address`,
+      },
+    },
+    phone: {
+      type: String,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return phonePattern.test(v);
+        },
+        message: (props) => `${props.value} is not a valid phone number`,
+      },
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const Contacts = model("contacts", contacts);
+
+module.exports = {
+  schema,
+  Contacts,
+};
--- server.js
+++ server.js
@@ -1,5 +1,22 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
+require("dotenv").config();
+mongoose.Promise = global.Promise;
+const PORT = process.env.PORT || 3000;
+const uriDb = process.env.DB_HOST;
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const connection = mongoose.connect(uriDb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+connection
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Database connection successful`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Server not running. Error message: ${err.message}`);
+    process.exit(1);
+  });
